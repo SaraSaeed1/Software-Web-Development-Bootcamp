a1ea9591919bcdd5967178664112e6fcf9cd7ef5
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-06-06 10:04:38 +0300

    Update Functions Intermediate I.py

--- python_stack/my_environments/Functions Intermediate I.py
+++ python_stack/my_environments/Functions Intermediate I.py
@@ -9,12 +9,6 @@ import random
 # If both a min and max number are provided, the function should return a random integer between those 2 values.
 # Here are a couple of important notes about using random.random() and rounding. (Create this function without using random.randInt() -- we are trying to build that method ourselves for this assignment!)
 
-# random.random() returns a random floating number between 0.000 and 1.000
-# random.random() * 50 returns a random floating number between 0.000 and 50.000
-# random.random() * 25 + 10 returns a random floating number between 10.000 and 35.000
-# round(num) returns the rounded integer value of num
-# Here's a little bit of code to get you started, with some test cases and expected outputs. Test each function call one at a time and a few times each to make sure you're getting the correct range.
-
 def randInt(min = 0,max = 100):
     if max >0 and min<max:
         if max != 100 and min != 0:
