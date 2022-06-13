70c3d28ca6ac031d8bf26c3ee6a38aec20ff9f1f
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-06-13 21:45:04 +0300

    Add files via upload

---
+++ python_stack/django/django_intro/multipleApps/usersApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path , include 
+from . import views
+
+urlpatterns = [
+    path('register', views.register),
+    path('login', views.login),
+    path('users/new', views.new),
+    path('users', views.users),
+
+    # path('/blogs', include('blog_app.urls')),
+    # path('/surveys', include('survey_app.urls')),
+    # path('', include('users_app.urls'))
+]
