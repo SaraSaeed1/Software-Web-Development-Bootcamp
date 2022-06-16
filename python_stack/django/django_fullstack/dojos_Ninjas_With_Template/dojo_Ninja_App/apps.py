aa5b21f67655383e940359584e1b6521c387a70d
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-06-16 16:48:04 +0300

    Delete python_stack/django/django_fullstack/dojos_Ninjas_With_Template directory

--- python_stack/django/django_fullstack/dojos_Ninjas_With_Template/dojo_Ninja_App/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DojoNinjaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dojo_Ninja_App'
--- python_stack/django/django_fullstack/dojos_Ninjas_With_Template/dojo_Ninja_App/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path     
-from .import views
-
-urlpatterns = [
-    path('', views.index),
-    path('addDojo', views.addDojo),
-    path('addNinja', views.addNinja),
-    path('<_id>', views.delete),
-]
