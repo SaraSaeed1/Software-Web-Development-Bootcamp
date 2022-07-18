7bd964ec3c0d5a0a3ebe2ff524b8379a95e919f6
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-07-18 20:47:51 +0300

    Add files via upload

---
+++ Java/greetings/Orders & Items/Order.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class Order{
+
+    public String name;
+    public double total;
+    public boolean ready;
+    // public ArrayList<String> item;
+    ArrayList<Item> item = new ArrayList<Item>();
+}
