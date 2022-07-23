3762342449af1b5e24c9859b458798ece86cdede
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-07-23 22:07:18 +0300

    Add files via upload

---
+++ Java/Spring/Calculator/src/com/calculator/one/TestCalculator.java
@@ -0,0 +1,11 @@
+package com.calculator.one;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		c.performOperation();
+		c.getResults();
+	}
+
+}
