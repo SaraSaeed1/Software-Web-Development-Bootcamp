e07281e68f83e3fea16bc92be714fed33c63024a
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-07-23 15:30:22 +0300

    Add files via upload

---
+++ Java/Spring/Zoo part2/src/com/zoo/two/Bat.java
@@ -0,0 +1,27 @@
+package com.zoo.two;
+
+public class Bat extends Mammal {
+	
+	public Bat() {
+		energyLevel =300;
+	}
+	
+	public void fly() {
+		System.out.println("fly");
+		energyLevel -=50;
+		displayEnergy();
+	}
+	
+	public void eatHumans() {
+		System.out.println("eatHumans");
+		energyLevel +=25;
+		displayEnergy();
+	}
+	
+	public void attackTown() {
+		System.out.println("attackTown");
+		energyLevel -=100;
+		displayEnergy();
+	}
+
+}
---
+++ Java/Spring/Zoo part2/src/com/zoo/two/Mammal.java
@@ -0,0 +1,9 @@
+package com.zoo.two;
+
+public class Mammal {
+	public int energyLevel=100;
+	
+	public void displayEnergy() {
+		System.out.println("The Energy Level is: " + energyLevel );
+	}
+}
