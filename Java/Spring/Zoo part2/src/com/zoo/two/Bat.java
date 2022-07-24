becf1dcffa447c29f6873c14eabc0cb54e927f3f
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-07-24 22:11:21 +0300

    Update Bat.java

--- Java/Spring/Zoo part2/src/com/zoo/two/Bat.java
+++ Java/Spring/Zoo part2/src/com/zoo/two/Bat.java
@@ -1,6 +1,7 @@
 package com.zoo.two;
 
 public class Bat extends Mammal {
+	int energyLevel= getEnergyLevel();
 	
 	public Bat() {
 		energyLevel =300;
@@ -9,18 +10,21 @@ public class Bat extends Mammal {
 	public void fly() {
 		System.out.println("Bat is flying!");
 		energyLevel -=50;
+		setEnergyLevel(energyLevel);
 		displayEnergy();
 	}
 	
 	public void eatHumans() {
 		System.out.println("so- well, never mind");
 		energyLevel +=25;
+		setEnergyLevel(energyLevel);
 		displayEnergy();
 	}
 	
 	public void attackTown() {
 		System.out.println("Bat is attacking town!");
 		energyLevel -=100;
+		setEnergyLevel(energyLevel);
 		displayEnergy();
 	}
 
