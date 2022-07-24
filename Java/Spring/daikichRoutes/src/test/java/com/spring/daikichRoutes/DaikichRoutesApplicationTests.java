34fb7adfec124a5ef725a0976e66bf0ca61fd5e7
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-07-24 11:03:07 +0300

    Add files via upload

---
+++ Java/Spring/daikichRoutes/src/test/java/com/spring/daikichRoutes/DaikichRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.daikichRoutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
