18d7a84499d835f11db01d044ed8dce1aabd5243
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-07-24 22:05:28 +0300

    Update Mammal.java

--- Java/Spring/Zoo Kepper Part1/src/com/zookeeper/one/Mammal.java
+++ Java/Spring/Zoo Kepper Part1/src/com/zookeeper/one/Mammal.java
@@ -1,13 +1,25 @@
 package com.zookeeper.one;
 
 public class Mammal {
-	public int energyLevel;
+	private int energyLevel;
 	
 	public Mammal () {
 		energyLevel=100;
 	}
 	
+	
+	public int getEnergyLevel() {
+		return energyLevel;
+	}
+
+
+	public void setEnergyLevel(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+
+
 	public void displayEnergy() {
 		System.out.println("The Energy Level is: " + energyLevel );
 	}
 }
+
