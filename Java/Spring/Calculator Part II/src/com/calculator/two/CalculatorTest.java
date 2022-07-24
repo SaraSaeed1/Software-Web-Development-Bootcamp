b3a43519c0eb31ea70fa6e9a6307d5697e7bb3f3
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-07-24 23:27:16 +0300

    Calculator Part II

---
+++ Java/Spring/Calculator Part II/src/com/calculator/two/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.calculator.two;
+import com.calculator.*;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c = new Calculator();
+		c.performOperation();
+
+	}
+
+}
