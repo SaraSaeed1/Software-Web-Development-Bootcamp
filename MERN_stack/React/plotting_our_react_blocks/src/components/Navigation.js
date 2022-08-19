87a74315d085c7420c99b08a9ffc91dac95e0365
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-08-19 15:59:14 +0300

    Add files via upload

---
+++ MERN_stack/React/plotting_our_react_blocks/src/components/Navigation.js
@@ -0,0 +1,12 @@
+import './comp.css';
+import React, {Component} from 'react'
+
+class Navigation extends Component{
+    render(){
+        return(
+            <div className="Navigation">
+            </div>
+        )
+    }
+}
+export default Navigation;
---
+++ MERN_stack/React/plotting_our_react_blocks/src/components/SubContents.js
@@ -0,0 +1,11 @@
+import './comp.css'
+import React, {Component} from 'react'
+class SubContents extends Component{
+    render(){
+        return(
+            <div className="SubContents">
+            </div>
+        )
+    }
+}
+export default SubContents;
