ec79fdac8299a02e410f3248a24cbdc80f25993b
Sara Alghamdi <66413283+SaraSaeed1@users.noreply.github.com>
2022-08-24 15:33:46 +0300

    Add files via upload

---
+++ MERN_stack/React/Pokemon/README.md
@@ -0,0 +1,25 @@
+# Pokemon API
+
+Using the previous lesson as a starting point, create a React project that simply has a button with the text "Fetch Pokemon".
+
+When this is clicked, output the names of all 807 Pokemon using the [Pokemon API. ](https://pokeapi.co)
+
+![](img.png)
+
+
+- [x] Create a button to fetch the Pokemon
+
+
+- [x] Output the names of all 807 Pokemon
+
+#
+
+To use the code
+- create a new react app called ``pokemon_api `` 
+
+```
+npx create-react-app pokemon_api
+```
+
+and then replace the ``src`` folder with [this folder]()
+
---
+++ MERN_stack/React/Pokemon/src/components/Pokemon.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Pokemon = () => {
+    const [people, setPeople] = useState([]);
+    const add=()=>{
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+            .then(response => response.json())
+            .then(response => setPeople(response.results))};
+    
+    return (
+        <div>
+            <button type="button" className="btn btn-outline-primary mt-4 mb-4" onClick={add} >Catch! </button>
+                {people.map((item,index)=>{
+                    return(
+                        <div key={index}>
+                            <ul>
+                                <li>
+                                    {item.name}
+                                </li>
+                            </ul>
+                        </div>
+                    )
+                })}
+        </div>
+    );
+}
+export default Pokemon;
